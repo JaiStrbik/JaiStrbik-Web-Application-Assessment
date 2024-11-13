329fdbc65152e116a3143306c7e690302ef07a3c
Jai <jstrbik@student.saintaug.nsw.edu.au>
2024-11-13 13:59:42 +1100

    Add database setup script and initial dashboard template

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
-from flask import request, redirect, url_for
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from werkzeug.security import check_password_hash
+from models import User
+
+from models import User, db_session
+app.secret_key = "supersecretkey" #Secure key for session security
 
 
 
@@ -21,17 +24,38 @@ def login():
 
         if user and check_password_hash(user.password, password):
          #store user information in the session
-         session['user_id'] = user.id
-         session['username'] = user.username
-         flash("Login sucessful!", "info")
-    
-    
-    
+            session['user_id'] = user.id
+            session['username'] = user.username
+            flash("Login sucessful!", "info")
+            return redirect(url_for("dashboard")) #redirect to the dashboard
+        else:
+            flash("Invalid username or password", "danger")
+        
     return render_template('login.html')
 
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
+
+
+
+@app.route('/dashboard')
+def dashboard():
+    #check if the user is loggin in by verifying the session data
+    if 'user_id' not in session:
+        flash("Please login to access the dsahboard", "warning")
+        return redirect(url_for("login"))
+    #render the dashboard template and pass the user's information from the session
+    return render_template('dashboard.html', user_id=session['user_id'], username=session['username'])
+
+@app.route('/logout')
+def logout():
+    #Clear all data in the session
+    session.clear()
+    flash("You have been logged out", "info")
+    return redirect(url_for("login"))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
